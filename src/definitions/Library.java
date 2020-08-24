f509d0a9cff910002d2a3298d37c92540b93f2d0
Parth Saraswat <parth.saraswat_cs19@gla.ac.in>
2020-08-25 00:10:30 +0530

    create an object of library class.

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -7,4 +7,18 @@
 package definitions;
 
 public class Library {
+
+// Components of a Definition Class in Java
+    // 1. private fields
+    // 2. public getters & setters
+    // 3. Constructors
+    // 4. toString() method
+    // 5. equals() and hashCodes() method
+
+    // rules for creating fields
+    // 1. Set the visibility modifier of the fields to private unless otherwise specified.
+    // 2. Set the access modifier of th fields to non-static unless otherwise specified.
+
+    private boolean[] Books;
+
 }
