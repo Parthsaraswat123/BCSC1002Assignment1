4e4c289b2a9e45cfb25a7cbf431ed1b6b4a0a8e8
Parth Saraswat <parth.saraswat_cs19@gla.ac.in>
2020-08-25 00:02:30 +0530

    create a objects of student class.

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -7,4 +7,27 @@
 package definitions;
 
 public class Student {
+
+    // Components of a Definition Class in Java
+    // 1. private fields
+    // 2. public getters & setters
+    // 3. Constructors
+    // 4. toString() method
+    // 5. equals() and hashCodes() method
+
+    // Student
+    // 1. a field to store the name of the student (first name, middle name, last name- all String).
+    //        2. a field to store the university roll number of the student (long).
+    //         3. a field to store the number of books issued by the student (int).
+    //        4. a field to store all the names of the books issued by the student (Book[]).
+
+    // rules for creating fields
+    // 1. Set the visibility modifier of the fields to private unless otherwise specified.
+    // 2. Set the access modifier of th fields to non-static unless otherwise specified.
+
+    private String studentName;
+    private long studentUniversityRollNumber;
+    private int studentIssuedBooksNumber;
+    private String[] studentIssuedBooksNames;
+
 }
