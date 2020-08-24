52483e191b67bdcde6358ccdb2e87d559db44bd0
Parth Saraswat <parth.saraswat_cs19@gla.ac.in>
2020-08-24 22:55:12 +0530

    create a objects of book class.

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -7,4 +7,17 @@
 package definitions;
 
 public class Book {
+
+    // Components of a Definition Class in Java
+    // 1. private fields
+    // 2. public getters & setters
+    // 3. Constructors
+    // 4. toString() method
+    // 5. equals() and hashCodes() method
+
+    private String bookName;
+    private String authorName;
+    private String bookISBNNumber;
+
+
 }
